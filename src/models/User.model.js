07f4a3f8783f478704eeb98323c4eb8d42e87b8b
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2026-02-26 18:01:28 +0530

    Profile pic update

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    profilePic: { type: String, default: "https://placehold.co/200x200" },
+    profilePic: { type: String, default: "https://res.cloudinary.com/darmatnf2/image/upload/v1772109026/user_pic_taeqah.png" },
     bio: { type: String, default: "Hey there! I am using CodeMates." },
     github: { type: String, default: "" },
     portfolio: { type: String, default: "" },
